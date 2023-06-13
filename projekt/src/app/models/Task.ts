68953179c21e99764f0ca6b8746603c05b8c5854
k-wos <kacper.wos33@gmail.com>
2023-06-13 20:06:26 +0200

    add Task model

---
+++ projekt/src/app/models/Task.ts
@@ -0,0 +1,13 @@
+export class Task {
+    id?: number;
+    name?: string;
+    desription?: string;
+    priority?: number;
+    functionalityId?: number;
+    estimatedTime?: Date;
+    status?: string;
+    createdDate?: Date;
+    startDate?: Date;
+    endDate?: Date;
+    assignedUSer?: string;
+}
