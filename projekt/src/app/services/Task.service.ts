78c793da3b1440a5799d255e7a6721cd43029f0e
k-wos <kacper.wos33@gmail.com>
2023-06-13 20:10:46 +0200

    add Task service

--- projekt/src/app/services/Task.service.ts
+++ projekt/src/app/services/Task.service.ts
@@ -1,10 +1,36 @@
+import { HttpClient } from '@angular/common/http';
 import { Injectable } from '@angular/core';
+import { Observable } from 'rxjs';
+import { Task } from '../models/Task';
 
 @Injectable({
   providedIn: 'root'
 })
 export class TaskService {
+  private baseUrl = 'http://localhost:3000/tasks';
 
-constructor() { }
+constructor(private http: HttpClient) { }
 
+getTasks(): Observable<Task[]> {
+  return this.http.get<Task[]>(this.baseUrl);
+}
+
+getTask(id: number): Observable<Task> {
+  const url = `${this.baseUrl}/${id}`;
+  return this.http.get<Task>(url);
+}
+
+addTask(task: Task): Observable<any> {
+  return this.http.post(this.baseUrl, task);
+}
+
+updateTask(task: Task): Observable<any> {
+  const url = `${this.baseUrl}/${task.id}`;
+  return this.http.put(url, task);
+}
+
+deleteTask(id: number): Observable<any> {
+  const url = `${this.baseUrl}/${id}`;
+  return this.http.delete(url);
+}
 }
